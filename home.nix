b2566ca79c07e9ae27f2888eddc39b092cade0c7
Joe Mitzman <joemitz@gmail.com>
2025-12-22 10:03:28 -0800

    generation 56 [2025-12-22 10:03]

--- home.nix
+++ home.nix
@@ -159,4 +159,13 @@
     categories = [ "AudioVideo" "Video" ];
     comment = "GTK UVC Viewer";
   };
+
+  xdg.desktopEntries.tiny4linux = {
+    name = "Tiny4Linux";
+    exec = "tiny4linux-gui";
+    icon = "camera-video";
+    terminal = false;
+    categories = [ "AudioVideo" "Video" ];
+    comment = "Control OBSBOT Tiny2 camera";
+  };
 }
