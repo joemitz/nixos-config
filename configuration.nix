385c1ac3da9e9ad886b2cab473c1eb335e91e362
Joe Mitzman <joemitz@gmail.com>
2025-12-21 16:12:34 -0800

    nixos: rebuild generation 41 [2025-12-21 16:12]

--- configuration.nix
+++ configuration.nix
@@ -182,6 +182,17 @@
         TIMELINE_LIMIT_MONTHLY = "12";   # Keep 12 monthly snapshots
         TIMELINE_LIMIT_YEARLY = "2";     # Keep 2 yearly snapshots
       };
+      root = {
+        SUBVOLUME = "/";
+        ALLOW_USERS = [ "joemitz" ];
+        TIMELINE_CREATE = true;
+        TIMELINE_CLEANUP = true;
+        TIMELINE_LIMIT_HOURLY = "48";    # Keep 48 hourly snapshots (2 days)
+        TIMELINE_LIMIT_DAILY = "7";      # Keep 7 daily snapshots
+        TIMELINE_LIMIT_WEEKLY = "4";     # Keep 4 weekly snapshots
+        TIMELINE_LIMIT_MONTHLY = "12";   # Keep 12 monthly snapshots
+        TIMELINE_LIMIT_YEARLY = "2";     # Keep 2 yearly snapshots
+      };
     };
   };
 
