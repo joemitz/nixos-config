1d2a1d6bd5624c46b70f1e5577bbb4bfc8dcd4ec
Joe Mitzman <joemitz@gmail.com>
2025-12-22 17:55:16 -0800

    generation 77 [2025-12-22 17:55]

--- home.nix
+++ home.nix
@@ -79,6 +79,12 @@
 
   programs.firefox.enable = true;
 
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    nix-direnv.enable = true;
+  };
+
   programs.bash = {
     enable = true;
     shellAliases = {
