60d0e063ac6bb53ae592e2b8e8edd94e3c04688d
Joe Mitzman <joemitz@gmail.com>
2025-12-29 09:14:29 -0800

    generation 116 [2025-12-29 09:14]

--- configuration.nix
+++ configuration.nix
@@ -282,17 +282,18 @@
     };
   };
 
-  # Mount NVMe drive
+  # Mount NVMe drive (read-only)
   fileSystems."/mnt/nvme" = {
     device = "/dev/disk/by-uuid/8590c09a-138e-4615-b02d-c982580e3bf8";
     fsType = "btrfs";
-    options = [ "subvol=@" ];
+    options = [ "subvol=@" "ro" ];
   };
 
-  # Mount TrueNAS Plex share via NFS
+  # Mount TrueNAS Plex share via NFS (read-only)
   fileSystems."/mnt/truenas/plex" = {
     device = "192.168.0.55:/mnt/main-pool/plex";
     fsType = "nfs";
+    options = [ "ro" ];
   };
 }
                                                                                                                                                                                                                  
