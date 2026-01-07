d44fb10f7548e76f9a9777cf3e1d8617db685a92
Joe Mitzman <joemitz@gmail.com>
2026-01-07 15:25:44 -0800

    generation 191 [2026-01-07 15:25]

--- system/boot.nix
+++ system/boot.nix
@@ -5,9 +5,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Use LTS kernel to avoid AMD GPU bug in kernel 6.12.10+
+  # Use 6.6 LTS kernel to avoid AMD GPU bug in kernel 6.12.10+
   # See: https://bbs.archlinux.org/viewtopic.php?id=303556
-  boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages_6_6;
 
   # Load AMD GPU driver early in boot (fixes display detection before SDDM starts)
   boot.initrd.kernelModules = [ "amdgpu" ];
