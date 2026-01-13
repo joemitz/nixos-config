4b285e41bfc741196a92d2e8b583d41e3dac82ae
Joe Mitzman <joemitz@gmail.com>
2026-01-13 12:36:10 -0800

    Gen 249: update kopia server credentials and auth configuration

--- system/kopia.nix
+++ system/kopia.nix
@@ -3,10 +3,10 @@
 {
   environment.systemPackages = [ pkgs.kopia ];
 
-  # Create credentials file for server control
+  # Create credentials file for HTTP basic auth
   environment.etc."kopia-server-control".text = ''
-    KOPIA_SERVER_CONTROL_USER=admin
-    KOPIA_SERVER_CONTROL_PASSWORD=password
+    KOPIA_SERVER_USERNAME=joemitz
+    KOPIA_SERVER_PASSWORD=kopia-local-pass
   '';
 
   systemd.services.kopia-server = {
@@ -20,7 +20,7 @@
       User = "root";
       Environment = "HOME=/root";
       EnvironmentFile = "/etc/kopia-server-control";
-      ExecStart = "${pkgs.kopia}/bin/kopia server start --address=https://127.0.0.1:51515 --tls-generate-cert";
+      ExecStart = "${pkgs.kopia}/bin/kopia server start --address=https://127.0.0.1:51515 --tls-generate-cert --server-username=$KOPIA_SERVER_USERNAME --server-password=$KOPIA_SERVER_PASSWORD";
 
       # Auto-restart if it crashes
       Restart = "on-failure";
