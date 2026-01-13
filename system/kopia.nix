212698c5f8c24e3351e6efcd7308a070eda35e2c
Joe Mitzman <joemitz@gmail.com>
2026-01-13 14:24:28 -0800

    Gen 254: simplify kopia server to remove unnecessary authentication

--- system/kopia.nix
+++ system/kopia.nix
@@ -1,18 +1,7 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 
 {
   environment.systemPackages = [ pkgs.kopia ];
-
-  # Create credentials file for HTTP basic auth and remote repository password
-  sops.templates."kopia-server-control" = {
-    owner = "root";
-    mode = "0400";
-    content = ''
-      KOPIA_SERVER_USERNAME=joemitz
-      KOPIA_SERVER_PASSWORD=kopia-local-pass
-      KOPIA_PASSWORD=${config.sops.placeholder.kopia_password}
-    '';
-  };
 
   systemd.services.kopia-server = {
     description = "Kopia backup server";
@@ -24,8 +13,7 @@
       Type = "simple";
       User = "root";
       Environment = "HOME=/root";
-      EnvironmentFile = config.sops.templates."kopia-server-control".path;
-      ExecStart = "${pkgs.kopia}/bin/kopia server start --address=https://127.0.0.1:51515 --tls-generate-cert --server-username=\${KOPIA_SERVER_USERNAME} --server-password=\${KOPIA_SERVER_PASSWORD}";
+      ExecStart = "${pkgs.kopia}/bin/kopia server start --address=https://127.0.0.1:51515 --tls-generate-cert";
 
       # Auto-restart if it crashes
       Restart = "on-failure";
