ce331ad585e817f6de2cdebd503fa1c0281dd7ea
Joe Mitzman <joemitz@gmail.com>
2026-01-22 10:49:58 -0800

    Gen 307: reorganize packages.nix with category comments for clarity

--- home/packages.nix
+++ home/packages.nix
@@ -2,36 +2,54 @@
 
 {
   home.packages = [
-    (pkgs-tiny4linux.callPackage ../pkgs/tiny4linux.nix { src = tiny4linux; })
-    claude-code.packages.x86_64-linux.default
-    pkgs.nerd-fonts.jetbrains-mono
-    pkgs.gh
-    pkgs.jq
+    
+    # AWS
     pkgs.awscli2
     pkgs.awslogs
-    pkgs.vscodium
-    pkgs.postman
-    pkgs.zoom-us
-    pkgs.teams-for-linux
-    pkgs.devbox
-    pkgs.tidal-hifi
-    pkgs.guvcview
-    pkgs.vlc
-    pkgs.gimp
-    pkgs.thunderbird
-    pkgs.remmina
-    pkgs.parsec-bin
+
+    # Android
     pkgs.android-studio
     pkgs.android-tools
     pkgs.jdk17
+
+    # Dev
+    claude-code.packages.x86_64-linux.default
+    pkgs.nerd-fonts.jetbrains-mono
+    pkgs.gh
+    pkgs.vscodium
     pkgs.nodejs_24
-    pkgs.btop
-    pkgs.eza
+    pkgs.devbox
+    pkgs.jq
+    pkgs.postman
+
+    # Nix
     pkgs.nixd
     pkgs.nixpkgs-fmt
     pkgs.nixf
     pkgs.statix
     pkgs.deadnix
     pkgs.sops
+
+    # Media
+    pkgs.tidal-hifi
+    pkgs.vlc
+
+    # Meetings
+    (pkgs-tiny4linux.callPackage ../pkgs/tiny4linux.nix { src = tiny4linux; })
+    pkgs.zoom-us
+    pkgs.guvcview
+
+    # Productivity
+    pkgs.teams-for-linux
+    pkgs.gimp
+    pkgs.thunderbird
+
+    # Remote Desktop
+    pkgs.remmina
+    pkgs.parsec-bin
+
+    # Terminal
+    pkgs.btop
+    pkgs.eza
   ];
 }
